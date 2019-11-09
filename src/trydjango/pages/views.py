a3af639d56fc447123a8d6add333a1425d4255ff
Nevadasurfer <jason@kennedyit.com>
2019-11-08 22:17:52 -0700

    added some templates

--- src/trydjango/pages/views.py
+++ src/trydjango/pages/views.py
@@ -2,8 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home_view(*args, **kwargs):
-    return HttpResponse("<h1>Hello World</h1>")
+def home_view(request, *args, **kwargs):
+    print(args, kwargs)
+    print(request.user)
+    #return HttpResponse("<h1>Hello World</h1>")
+    return render(request, "home.html", {})
 
-def contact_view(*args, **kwargs):
-    return HttpResponse("<h1>Contact Page</h1>")
+def contact_view(request, *args, **kwargs):
+    print(args, kwargs)
+    print(request.user)
+    #return HttpResponse("<h1>Hello World</h1>")
+    return render(request, "contact.html", {})
+
+def about_view(request, *args, **kwargs):
+    print(args, kwargs)
+    print(request.user)
+    #return HttpResponse("<h1>Hello World</h1>")
+    return render(request, "about.html", {})
